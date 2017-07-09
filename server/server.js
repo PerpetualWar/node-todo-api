29a143bd34f955208409432b4dfa74153603b814
PerpetualWar <perpetualwarr@gmail.com>
2017-07-09 15:52:30 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -16,10 +16,12 @@ app.post('/todos', (req, res) => {
   todo.save().then((docs) => {
     res.send(docs);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
-})
+});
+
+module.exports = {app};
