712812251a4011794b6a618e4a41a11bb2959fe6
PerpetualWar <perpetualwarr@gmail.com>
2017-07-09 19:49:04 +0200

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+
+var db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://perp:metal1@ds041494.mlab.com:41494/node-todo-app'
+};
+mongoose.connect(db.localhost || db.mlab, {
   useMongoClient: true
 });
 
