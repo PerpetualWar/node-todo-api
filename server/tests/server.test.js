19023bd42873d4db44341963162e703ae92585e9
PerpetualWar <perpetualwarr@gmail.com>
2017-08-22 20:06:48 +0200

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,7 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
-const {User} = require('./../models/user');
+const { User } = require('./../models/user');
 const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 beforeEach(populateUsers);
@@ -127,24 +127,24 @@ describe('DELETE /todos/:id', () => {
 describe('GET /users/me', () => {
   it('should return user if authenticated', (done) => {
     request(app)
-    .get('/users/me')
-    .set('x-auth', users[0].tokens[0].token)
-    .expect(200)
-    .expect((res) => {
-      expect(res.body._id).toBe(users[0]._id.toHexString());
-      expect(res.body.email).toBe(users[0].email);
-    })
-    .end(done);
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
   });
 
   it('should return a 401 if not authenticated', (done) => {
     request(app)
-    .get('/users/me')
-    .expect(401)
-    .expect((res) => {
-      expect(res.body).toEqual({});
-    })
-    .end(done);
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
   });
 });
 
@@ -155,7 +155,7 @@ describe('POST /users', (done) => {
 
     request(app)
       .post('/users')
-      .send({email, password})
+      .send({ email, password })
       .expect(200)
       .expect((res) => {
         expect(res.headers['x-auth']).toExist();
@@ -166,27 +166,78 @@ describe('POST /users', (done) => {
         if (err) {
           return done(err);
         }
-        User.findOne({email}).then((user) => {
+        User.findOne({ email }).then((user) => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
   it('should return validatiojn errors if request invalid', (done) => {
     request(app)
-    .post('/users/')
-    .send({ email: 'r', password: '!' })
-    .expect(400)
-    .end(done);
+      .post('/users/')
+      .send({ email: 'r', password: '!' })
+      .expect(400)
+      .end(done);
   });
 
   it('should not create user if email in use', (done) => {
     request(app)
-    .post('/users/')
-    .send({ email: 'srle@srle.com', password: 'userOnePass' })
-    .expect(400)
-    .end(done);
-  }); 
+      .post('/users/')
+      .send({ email: 'srle@srle.com', password: 'userOnePass' })
+      .expect(400)
+      .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + '1'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
